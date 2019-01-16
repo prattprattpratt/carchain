c92e49d583489147c28ce9b050a91e1981c2104f
Chris Pratt <chris@neiybor.com>
2019-01-16 12:27:30 -0700

    clean up logo

--- index2.js
+++ index2.js
@@ -9,3 +9,27 @@ window.addEventListener("scroll", ev => {
 
     hero2LayerTwo.style.transform = `rotate(${hero2Deg}deg)`;
 });
+
+// let interval;
+
+// window.addEventListener("mouseover", ev => {
+//     const target = document.getElementById(ev.target.id);
+
+//     if (target) {
+//         let offset = 10;
+//         interval = window.setInterval(() => { 
+//             offset += 10;
+//             target.style.strokeDasharray = "40px";
+//             target.style.strokeDashoffset = `${offset}px`;
+//         }, 100);
+//     }
+// });
+
+// window.addEventListener("mouseout", ev => {
+//     const target = document.getElementById(ev.target.id);
+
+//     if (target) {
+//         window.clearInterval(interval);
+//         target.style.strokeDasharray = "0";
+//     }
+// });
